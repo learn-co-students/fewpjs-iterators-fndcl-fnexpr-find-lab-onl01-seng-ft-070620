ab125d11c06478c1f31223a50ce9ef94d8288f9b
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-16 12:06:35 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array)
+{
+  let object = array.find(function(e){return e.result === "W" })
+  if(object != undefined)
+  {
+    return object.year
+  }
+  else
+  {
+    return object
+  }
+}
