dec3ab4159e863c64e535b7f2b523306c3ae20d4
cTraymany <chindalatht@gmail.com>
2020-10-17 15:00:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// pry = require('pryjs');
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+    const winner = array.find(obj => {
+    return obj['result'] === "W"
+  });
+
+  if (winner) {
+    return winner['year']
+  }
+}
+
+// superbowlWin([
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ])
