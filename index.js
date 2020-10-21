6392494127d4452ce28c8fc9ee273e5786a1cbfb
rayalva407 <rayalva407@gmail.com>
2020-10-21 08:44:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const testVar = {}
 
+function superbowlWin(arr) {
+ let result = arr.find(obj => obj.result === "W")
+ if (result) {
+   return result.year
+ }
+}
+
 function testFunc() {
   return "hi"
 }
