c3865e4f8a4ea1d235a8369e3a96c93c64e7c2f5
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-10-22 17:33:03 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(obj => { return obj.result === 'W' })
+  if (win) {
+    return win.year
+  }
+}
